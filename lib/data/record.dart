f1aa5f9275635a2cbeb1271bd4660b6abf16876f
nauss <bpommier@gmail.com>
2021-08-31 11:21:28 +0200

    init

---
+++ lib/data/record.dart
@@ -0,0 +1,34 @@
+import 'package:hive/hive.dart';
+import 'package:selector/data/constants.dart';
+import 'package:selector/data/debug.dart';
+import 'package:selector/data/hive_ids.dart';
+import 'package:selector/data/record_info.dart';
+import 'package:selector/data/enums.dart';
+
+part 'record.g.dart';
+
+@HiveType(typeId: hiveRecordId)
+class Record extends HiveObject {
+  static const BoxName = 'records';
+
+  @HiveField(0)
+  RecordStatus status;
+  @HiveField(1)
+  int position;
+  @HiveField(2)
+  RecordInfo info;
+
+  Record({
+    required this.info,
+    this.status = RecordStatus.missing,
+    this.position = -1,
+  });
+
+  Future<void> store() async {
+    // Get the box
+    var box = Hive.box(BoxName);
+    await box.put(position, this);
+  }
+}
+
+typedef RecordList = List<Record>;
