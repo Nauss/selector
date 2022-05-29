a09a17551bb5bbb7c938ebdcf3653b5fbe2c3fae
nauss <bpommier@gmail.com>
2022-05-29 20:20:13 +0200

    fix: record screen is now smoother

--- lib/screens/record_screen.dart
+++ lib/screens/record_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sticky_header/flutter_sticky_header.dart';
 import 'package:get_it/get_it.dart';
 import 'package:selector/data/constants.dart';
 import 'package:selector/data/discogs.dart';
@@ -7,6 +8,8 @@ import 'package:selector/data/record.dart';
 import 'package:selector/data/utils.dart';
 import 'package:selector/widgets/record_buttons.dart';
 import 'package:selector/widgets/tracks.dart';
+
+const iconSize = 15.0;
 
 class RecordScreen extends StatelessWidget {
   final Discogs discogs = GetIt.I.get<Discogs>();
@@ -30,75 +33,85 @@ class RecordScreen extends StatelessWidget {
               stream: discogs.recordDetailStream,
               builder: (context, snapshot) {
                 final updatedInfo = snapshot.data?.info ?? recordInfo;
-                return Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Flexible(
-                      flex: 2,
-                      child: Center(
-                        child: Stack(
-                          children: [
-                            Hero(
-                              tag: Tags.cover(record.uniqueId),
-                              child: Image(
-                                image: getImage(updatedInfo),
-                              ),
-                            ),
-                            if (record.status != RecordStatus.missing)
-                              Positioned(
-                                right: 4,
-                                bottom: 4,
-                                child: CircleAvatar(
-                                  radius: 19,
-                                  backgroundColor:
-                                      themeData.dialogBackgroundColor,
-                                  child: record.status == RecordStatus.outside
-                                      ? SVGs.listening(
-                                          color: themeData.primaryColor,
-                                        )
-                                      : SVGs.mySelector(
-                                          color: themeData.primaryColor,
-                                        ),
+                return CustomScrollView(
+                  slivers: <Widget>[
+                    SliverStickyHeader(
+                      header: Container(
+                        decoration: BoxDecoration(
+                          color: themeData.scaffoldBackgroundColor,
+                        ),
+                        child: Center(
+                          child: Stack(
+                            children: [
+                              Hero(
+                                tag: Tags.cover(record.uniqueId),
+                                child: Image(
+                                  image: getImage(updatedInfo),
                                 ),
-                              )
-                          ],
+                              ),
+                              if (record.status != RecordStatus.missing)
+                                Positioned(
+                                  right: 4,
+                                  bottom: 4,
+                                  child: CircleAvatar(
+                                    radius: iconSize,
+                                    backgroundColor:
+                                        themeData.dialogBackgroundColor,
+                                    child: record.status == RecordStatus.outside
+                                        ? SVGs.listening(
+                                            color: themeData.primaryColor,
+                                            height: iconSize,
+                                            width: iconSize,
+                                          )
+                                        : SVGs.mySelector(
+                                            color: themeData.primaryColor,
+                                            height: iconSize,
+                                            width: iconSize,
+                                          ),
+                                  ),
+                                )
+                            ],
+                          ),
+                        ),
+                      ),
+                      sliver: SliverToBoxAdapter(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                updatedInfo.title,
+                                style: themeData.textTheme.headline4,
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              Text(
+                                updatedInfo.artist,
+                                style: themeData.textTheme.subtitle1,
+                              ),
+                              Text(
+                                "${updatedInfo.year} - ${updatedInfo.country}",
+                                style: themeData.textTheme.bodyText1,
+                              ),
+                              Text(
+                                updatedInfo.format,
+                                style: themeData.textTheme.bodyText1,
+                              ),
+                              Text(
+                                updatedInfo.label,
+                                style: themeData.textTheme.bodyText1,
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            updatedInfo.title,
-                            style: themeData.textTheme.headline4,
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                          Text(
-                            updatedInfo.artist,
-                            style: themeData.textTheme.subtitle1,
-                          ),
-                          Text(
-                            "${updatedInfo.year} - ${updatedInfo.country}",
-                            style: themeData.textTheme.bodyText1,
-                          ),
-                          Text(
-                            updatedInfo.format,
-                            style: themeData.textTheme.bodyText1,
-                          ),
-                          Text(
-                            updatedInfo.label,
-                            style: themeData.textTheme.bodyText1,
-                          ),
-                        ],
+                    SliverStickyHeader(
+                      header: RecordButtons(record: record),
+                      sliver: SliverToBoxAdapter(
+                        child: Tracks(tracks: updatedInfo.tracks),
                       ),
-                    ),
-                    RecordButtons(record: record),
-                    Flexible(
-                      flex: 1,
-                      child: Tracks(tracks: updatedInfo.tracks),
                     ),
                   ],
                 );
